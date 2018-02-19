733a97cec73eb6fecdc667d6597d2b363bb15f0d
noamr <noam.raby@sundaysky.com>
2018-02-19 21:45:38 +0200

    test react project

---
+++ 2018-02-19/first-project/src/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const footer = (props) => {
+    return (
+        <div>
+            <p>{props.title}</p>
+            <button>test</button>
+        </div>
+    );
+}
+
+export default footer;
---
+++ 2018-02-19/first-project/src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = (props) => {
+    return (
+        <div>
+            <p>{props.title}</p>
+            <button>test</button>
+        </div>
+    );
+}
+
+export default header;
